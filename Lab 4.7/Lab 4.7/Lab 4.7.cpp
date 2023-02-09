d9c7f4fcfc38a585d9f761739b0ffd71eb4cf88e
RostyslavIvato <90511382+rostyk5041@users.noreply.github.com>
2023-02-09 13:28:06 +0200

    Add files via upload

---
+++ Lab 4.7/Lab 4.7/Lab 4.7.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double y ,xp, xk, x, dx, eps, a=0, R = 0, S = 0;
+	int n = 0;
+	
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "ln(1-x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		y = log1p(-x);
+		n = 1; // вираз залежить від умови завдання варіанту
+		a = x;
+		S = a;
+		do {
+			n++;
+			R = (x*n-x)/n; // вираз залежить від умови завдання варіанту
+			a *= R;
+			S += a;
+		}
+		while (abs(a)>=eps);
+		S *= -1;
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << y << " |"
+			<< setw(10) << setprecision(5) << S << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
---
+++ Lab 5.2/Lab 5.2/Lab 5.2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void S(const double x, const double eps, int &n, double &s);
+void A(const double x, const int n, double &a);
+
+int main()
+{
+	double y, xp, xk, x, dx, eps, R=0, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps ="; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "ln(1-x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		y = log1p(-x);
+		S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << y << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+void S(const double x, const double eps, int &n, double &s)
+{
+	n = 1; // вираз залежить від умови завдання варіанту
+	double a = -x; // вираз залежить від умови завдання варіанту
+	s = a;
+	
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+}
+void A(const double x, const int n, double &a)
+{
+	double R = (x*n -x)/ n; // вираз залежить від умови завдання варіанту
+	a *= R;
+	}
