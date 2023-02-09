8403f609cd13491df3a0e02e3e5449ab53f0e9ae
RostyslavIvato <90511382+rostyk5041@users.noreply.github.com>
2023-02-09 13:24:16 +0200

    Add files via upload

---
+++ Lab 3.4/Lab 3.4/Lab 3.4.cpp
@@ -0,0 +1,23 @@
+﻿// Іватьо Р.М Ік-11
+// Лабораторна робота № 3.4
+// Варіант 6
+#include <iostream>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+	double y; // вхідний параметр
+	double R; // вхідний параметр
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	// розгалуження в повній формі
+	if (((y + R) * (y + R) >= R * R - ((x + R) * (x + R)) && y <= 0 && x >= -R && x <= 0)
+		|| (y * y <= (R * R - ((x - R) * (x - R))) && y >= 0))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+	cin.get();
+	return 0;
+}
---
+++ Lab 4.1/Lan4.1/Lan4.1.cpp
@@ -0,0 +1,40 @@
+﻿// Іватьо Ростислав ІК-11
+//Lab 4.1
+//Варіант 6
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N;
+	double P;
+	cout << "N = "; cin >> N;
+	P = 1;
+	k = N;
+	while (k <= 19)
+	{
+		P *= (1. * k - N) / (1.*k + N) + 1;
+		k++;
+	}
+	cout << P << endl;
+	P = 1;
+	k = N;
+	do {
+		P *= (1. * k - N) / (1. * k + N) + 1;
+		k++;
+	} while (k <= 19);
+	cout << P << endl;
+	P = 1;
+	for (k = N; k <= 19; k++)
+	{
+		P *= (1. * k - N) / (1.* k + N) + 1;
+	}
+	cout << P << endl;
+	P = 1;
+	for (k = 19; k >= N; k--)
+	{
+		P *= (1. * k - N) / (1.*k + N) + 1;
+	}
+	cout << P << endl;
+	return 0;
+}
